a0d7b1818f0992f5c7be2ff83a7862dc2f331def
Amar Raj <theme>
2026-06-07 20:48:58 +0530

    02-Components committing

---
+++ 02-Component/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// Various-Components :- 
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Header from './components/header'
+
+const App = () => {
+  return (
+    <>
+      <h1>Lets Learn About Components....</h1>
+      <Header/>
+      <Navbar/>
+      <Footer/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ 02-Component/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h3>hello from header.jsx</h3>
+    </div>
+  )
+}
+
+export default Header
