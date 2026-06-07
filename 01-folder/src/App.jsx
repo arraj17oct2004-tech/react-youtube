b79633e283079b0c42cf38212322ef32330813e2
Amar Raj <theme>
2026-06-07 20:00:34 +0530

    initial commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+function App(){
+  return  <> 
+      <div id="chacha"><h1>Learning About Fragments</h1></div>
+      <div id = "dad">
+            <h2 id = "child-1" > child-1 </h2>
+            <h2 id="child-2">child-2</h2>
+      </div>
+  </>
+  
+
+}
+
+// fragments html ko render krte time nai dikhte
+export default App
+
